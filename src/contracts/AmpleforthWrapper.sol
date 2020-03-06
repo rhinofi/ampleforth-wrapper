56cd14654828106a07052ec7b46a947d16a76957
rzadp <przemek@ethworks.io>
2020-03-06 16:15:49 +0100

    Added methods to be overriden

--- src/contracts/AmpleforthWrapper.sol
+++ src/contracts/AmpleforthWrapper.sol
@@ -6,4 +6,19 @@ contract AmpleforthWrapper is WrapperLock {
     constructor(address _originalToken) WrapperLock(_originalToken, "Ampleforth Wrapper", "AMPL", 9) {
 
     }
+
+    // @override
+    function balanceOf(address _owner) public constant returns (uint256) {
+        revert("Should return balance in AMPL (not gons)");
+    }
+
+    // @override
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        revert("Value should be in AMPL (not gons)");
+    }
+
+    // @override
+    function transferFrom(address _from, address _to, uint _value) public {
+        revert("Value should be in AMPL (not gons)");
+    }
 }
